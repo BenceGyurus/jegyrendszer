15a091ac3a3b8abc3dcfbe3d649ff3f00ca2b69f
BenceGyurus <81524938+BenceGyurus@users.noreply.github.com>
2023-01-31 00:37:46 +0100

    docker

--- public/js/animation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const loadAnimation = (className) => {
-    let elements = document.querySelectorAll(`.${className}`);
-    let i = 0;
-    elements.forEach(element => {
-        element.style.animationDelay = `${i / 10}s`;
-        i++;
-    });
-};
--- src/animation.ts
+++
@@ -1,8 +0,0 @@
-const loadAnimation = (className)=>{
-    let elements = document.querySelectorAll(`.${className}`);
-    let i = 0;
-    elements.forEach(element => {
-        element.style.animationDelay = `${i/10}s`
-        i++;
-    });
-}
