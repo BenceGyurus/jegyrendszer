b397ed7150fb3744526edce7d5b171dca8d56117
BenceGyurus <81524938+BenceGyurus@users.noreply.github.com>
2024-02-09 16:39:59 +0100

    mongo.js

--- handle-server/mongo/mongo.js
+++ handle-server/mongo/mongo.js
@@ -2,32 +2,34 @@ const fs = require("fs");
 const { MongoClient } = require("mongodb");
 const Function = require("../functions.js");
 
-const config = JSON.parse(fs.readFileSync(`${process.env.CONFIGDIR}/config.json`));
+const config = JSON.parse(
+  fs.readFileSync(`${process.env.CONFIGDIR}/config.json`),
+);
 
-class Database{
-  constructor(name){
+class Database {
+  constructor(name) {
     let datas = Function.getNameOfDatabase(name);
-    this.collectionName = datas.collection; this.databaseName = datas.database;
+    this.collectionName = datas.collection;
+    this.databaseName = datas.database;
     this.mongoconfig = {};
-    if (!this.mongoconfig) return {error : true};
-    this.client = new MongoClient(process.env.NODE_ENV == 'production' ? "mongo:27017" : config['MONGO_URI']);
+    if (!this.mongoconfig) return { error: true };
+    this.client = new MongoClient(config["MONGO_URI"]);
     this.Db();
     let db = this.client.db(this.databaseName);
     this.createCollection();
     this.collection = db.collection(this.collectionName);
-    return {collection : this.collection, database : this.client};
+    return { collection: this.collection, database: this.client };
   }
 
-  async createCollection(){
-    try{
+  async createCollection() {
+    try {
       await this.client.createCollection(this.collection);
-    }
-    catch{
+    } catch {
       return false;
     }
   }
 
-  async Db(){
+  async Db() {
     await this.client.connect();
   }
 }
