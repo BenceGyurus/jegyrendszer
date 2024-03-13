0b7c298fd498f131e62aee0d24165ee95c929adc
BenceGyurus <81524938+BenceGyurus@users.noreply.github.com>
2024-03-13 20:28:47 +0100

    user access skeleton

--- web-server/src/components/admin/accessSkeleton.component.tsx
+++ web-server/src/components/admin/accessSkeleton.component.tsx
@@ -2,8 +2,8 @@ import Skeleton from '@mui/material/Skeleton';
 
 const AccessSkeleton = ()=>{
     return(
-        <div style={{display : "flex", justifyContent : "space-between", width : "180px", marginBottom : 10, marginLeft: 20}}>
-        <Skeleton animation="wave" width = {Math.random() * (150-50) + 50} height = {40} />
+        <div style={{display : "flex", justifyContent : "space-between", width : "30px", marginBottom : 10, marginLeft: 20}}>
+        <Skeleton animation="wave" width = {30} height = {40} />
         </div>
     )
 }
