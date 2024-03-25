0d09f9f198ff798146ac629cca0fa593a605d9e0
BenceGyurus <81524938+BenceGyurus@users.noreply.github.com>
2024-03-25 12:18:51 +0100

    asdasdkamsdlgjaoirgjaeoirgj

--- handle-server/simple_signature.js
+++ handle-server/simple_signature.js
@@ -1,10 +1,13 @@
 const CryptoJS = require("crypto-js");
 
-//const signWithCryptoJS = (data) => cryptoJs.enc.Base64.stringify(cryptoJs.HmacSHA384(JSON.stringify(data).replace(/\//g, "\\\/"), process.env.MERCHANT_KEY))
+
 const signWithCrypto = (data) =>  {
-    const hash = CryptoJS.HmacSHA384(JSON.stringify(data), process.env.MERCHANT_KEY);
-    let othersignautre = hash.toString(CryptoJS.enc.Base64)
-    return othersignautre
+    if (process.env.MERCHANT_KEY){
+        const hash = CryptoJS.HmacSHA384(JSON.stringify(data), process.env.MERCHANT_KEY);
+        let othersignautre = hash.toString(CryptoJS.enc.Base64)
+        return othersignautre
+    }
+    return false;
 }
 
 module.exports = signWithCrypto;
